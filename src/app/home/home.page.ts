cd98e5a0e27f526844cfce6b15eac53e3c5594c0
Kevin Hernandez Avelino <kevHernandez05@hotmail.com>
2021-07-11 14:17:55 -0400

    trayendo data desde direccion eth dada

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -16,15 +16,50 @@ export class HomePage {
   private movements: IMovement[];
   private web3: any;
 
-  address: any = '0xc8F595E2084DB484f8A80109101D58625223b7C9';
+  private account: IMovement;
+
+  infuraUrl:string = "https://mainnet.infura.io/v3/7f1789c7008f4f02b8c53ab149849ccb"
+  address: any = '0xEDaE3708C601E7F1b80F2733138150E02ce18eeA';
   balance: any;
   ether: number = 0;
   euro: number = 0;
 
   constructor() {
     //this.movements = this.mockMovements();
-
+    let conn = this.web3jsInit(this.infuraUrl);
+    console.log(`Connetion state: ${conn}`);
   }
+
+  web3jsInit(infuraUrl:string): boolean{
+    this.web3 = new Web3(infuraUrl);
+    this.getBalance(this.address);
+    return true;
+  }
+
+  getBalance(address: string){
+    this.web3.eth.getBalance(this.address, (err, bal)=>{
+      if(bal !== undefined){
+
+        let balance = this.web3.utils.fromWei(bal, 'ether');
+
+        this.account =
+          {
+            cuentaEth: this.address,
+            balance: balance,
+            date: '',
+            percentage: ''
+          }
+        ;
+      }
+    });
+  }
+
+  getTransactions(address: string){
+    this.web3.eth.getTransaction(this.address, (err, tran)=>{
+    });
+  }
+
+
 
   mockMovements(): IMovement[]{
     return [
